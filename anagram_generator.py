e016e70a9613a1da87fe730d983ae8e42930fb9d
Jayce Azua <jayce.azua@gmail.com>
2018-11-08 09:47:43 -0800

    fixing my naming

--- anagram_generator.py
+++ anagram_generator.py
@@ -1,16 +1,16 @@
-def all_perms(word):
+def anagram(word):
    if len(word) <= 1:
        return word
    else:
        possible_words = []
-       for perm in all_perms(word[1:]): # using the power of recursion
+       for perm in anagram(word[1:]): # using the power of recursion
            for i in range(len(word)): # loop obvs lol
                possible_words.append(perm[:i] + word[0:1] + perm[i:])
        return possible_words
 
 def main():
     word = "war"
-    print(all_perms(word))
+    print(anagram(word))
 
 if __name__ == "__main__":
     main()
