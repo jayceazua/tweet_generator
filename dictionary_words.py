5b871f8c02d39e7900835a429acc073440a4261c
Jayce Azua <jayce.azua@gmail.com>
2018-11-09 01:19:33 -0800

    finished with dictionary words

--- dictionary_words.py
+++ dictionary_words.py
@@ -10,10 +10,40 @@ def get_words():
     # use splitlines() to now allow that.
     return dictionary_words.splitlines()
 
-def user_input(int):
+def user_input():
     """ Make sure that the user is putting in an integer """
     user_input = sys.argv[1]
     if type(user_input) != 'int':
         return 'Invalid Input; please input a integer.'
     else:
         return user_input
+
+def random_words_arrange(input_int, list_words):
+	"""
+	This function uses the words from a file
+		and generates a random "sentence"
+	"""
+	# I am getting the total length of the file.
+	len_file = len(get_words())
+	# declare an empty list
+	words_list = []
+	# stores the sentence here
+	sentence = ""
+		# the condition is when
+	while input_int != 0:
+		# generates a random number based on the length of the file
+		rand_num = random.randint(0, len_file -1)
+		# it will add the random selected word into our empty list
+		words_list.append(list_words[rand_num] + " ")
+		# prevents from getting the same word
+		list_words.pop(rand_num)
+		# subtract by one to loop again
+		input_int -= 1
+
+	return sentence.join(words_list)
+
+if __name__ == "__main__":
+    list_words = get_words()
+    # print(list_words)
+    input_int = int(sys.argv[1])
+    print(random_words_arrange(input_int, list_words))
