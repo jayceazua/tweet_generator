1b8bfcf1ab882067609c7dbcfe26564cc2c81a6d
Jayce Azua <jayce.azua@gmail.com>
2018-11-10 23:09:56 -0800

    almost done

--- analyze_word.py
+++ analyze_word.py
@@ -85,41 +85,6 @@ if __name__ == "__main__":
 
 
 """
-# Step 2. Listogram
-def frequency_list(word, histogram):
-	# lower case the word input
-	word_use = word.lower()
-    # loop through the list of lists
-	for item in histogram:
-		# check if the word input is in the dictionary
-		if word_use in item[0]:
-			# if so return the value
-			return item[1]
-
-########## ########## ########## ##########
-# 1. What is the least/most frequent word(s)?
-# 2. How many different words are used?
-def uniqie_words (histogram):
-	return len(histogram)
-# 3. What is the average (mean/median/mode) frequency of words in the text?
-
-def source_text(corpus):
-	"
-        This function gets a corpus and splits it into a list.
-	"
-	return corpus.lower().split()
-
-def source_text_len():
-	"
-	    This function gets the length of the corpus after splitting into a list.
-	"
-	return len(source_text())
-
-
-
-########## ########## ########## ##########
-
-
 ########## ########## ########## ##########
 # Testing
 def test_relative():
@@ -187,74 +152,11 @@ class Dictogram(object):
 				return key
 
 
-
 if __name__ == "__main__":
 	x = "One dog two dog red dog blue dog"
 	test = Dictogram(file_corpus)
 	print(test.histogram)
 	print(test.tokens)
 	print(test.random_word())
-
-# listogram:
-class Listogram(list):
-    "Listogram is a histogram implemented as a subclass of the list type."
-
-    def __init__(self, word_list=None):
-        "Initialize this histogram as a new list and count given words."
-        super(Listogram, self).__init__()  # Initialize this as a new list
-        # Add properties to track useful word counts for this histogram
-        self.types = 0  # Count of distinct word types in this histogram
-        self.tokens = 0  # Total count of all word tokens in this histogram
-        # Count words in given list, if any
-        if word_list is not None:
-            for word in word_list:
-                self.add_count(word)
-
-    def add_count(self, word, count=1):
-        "Increase frequency count of given word by given count amount."
-        # TODO: Increase word frequency by count
-
-    def frequency(self, word):
-        "Return frequency count of given word, or 0 if word is not found.
-        # TODO: Retrieve word frequency count
-
-    def __contains__(self, word):
-        "Return boolean indicating if given word is in this histogram."
-        # TODO: Check if word is in this histogram
-
-    def _index(self, target):
-        "Return the index of entry containing given target word if found in
-        this histogram, or None if target word is not found."
-        # TODO: Implement linear search to find index of entry with target word
-
-
-def print_histogram(word_list):
-    print('word list: {}'.format(word_list))
-    # Create a listogram and display its contents
-    histogram = Listogram(word_list)
-    print('listogram: {}'.format(histogram))
-    print('{} tokens, {} types'.format(histogram.tokens, histogram.types))
-    for word in word_list[-2:]:
-        freq = histogram.frequency(word)
-        print('{!r} occurs {} times'.format(word, freq))
-    print()
-
-
-def main():
-    import sys
-    arguments = sys.argv[1:]  # Exclude script name in first argument
-    if len(arguments) >= 1:
-        # Test histogram on given arguments
-        print_histogram(arguments)
-    else:
-        # Test histogram on letters in a word
-        word = 'abracadabra'
-        print_histogram(list(word))
-        # Test histogram on words in a classic book title
-        fish_text = 'one fish two fish red fish blue fish'
-        print_histogram(fish_text.split())
-        # Test histogram on words in a long repetitive sentence
-        woodchuck_text = ('how much wood would a wood chuck chuck'
-                          ' if a wood chuck could chuck wood')
-        print_histogram(woodchuck_text.split())
-    """
+    
+"""
