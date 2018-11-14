775de5408f9ab1fc5df6ba2eeeb2dc3f0a52e20f
Jayce Azua <jayce.azua@gmail.com>
2018-11-14 12:21:53 -0800

    finsihed deploying

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+# import the files needed to generate sentences
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # return for custom code 
     return "Hello Jasmine!"
 
 @app.route('/tweeter')
